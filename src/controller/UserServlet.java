6825a3cc61469f1532d9433b223c466a886a287c
Elsin14 <elsin.drafus14@gmail.com>
2020-10-27 10:20:32 -0500

    four commit

--- src/controller/UserServlet.java
+++ src/controller/UserServlet.java
@@ -11,13 +11,13 @@ import javax.servlet.http.HttpServletResponse;
  * Servlet implementation class LibroServlet
  */
 @WebServlet("/LibroServlet")
-public class LibroServlet extends HttpServlet {
+public class UserServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public LibroServlet() {
+    public UserServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
